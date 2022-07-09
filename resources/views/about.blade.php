ec91af03082f14bbc85d09f5dbd2cb9557bc0bdb
Kizuxy <berliferizadam16@gmail.com>
2022-07-09 15:14:30 +0700

    laravel

---
+++ resources/views/about.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('container')
+<body>
+    <h1>Hal About</h1>
+    <h3>{{ $name }}</h3>
+    <p>{{ $email }}</p>
+</body>
+</html>
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('container')
+
+<h1 style= "font-family: Secular One" >BERLIAN</h1>
+
+@endsection
